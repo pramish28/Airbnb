e65272e2236896acc8d8053114935e6fd807e33c
pramish28 <pramishsharma78@gmail.com>
2024-05-09 01:22:30 +0545

    mapping of data

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
+import { useState } from "react";
+import Navbar from "./Navbar.jsx";
+import Hero from "./Hero.jsx";
+import Card from "./Card.jsx";
+import Data from "./Data.jsx";
 export default function App() {
   return (
-    <div>
-    </div>
-     
- 
-  )
+    <>
+      <Navbar />
+      <Hero />
+     <Data />
+    </>
+  );
 }
-
-
-
--- project1/src/main.jsx
+++ project1/src/main.jsx
@@ -6,16 +6,6 @@ import Hero from './Hero.jsx'
 import Card from './Card.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Navbar />
-    <Hero />
-    <Card 
-      img="katie-zaferes.png"
-      rating="5.0"
-      reviewCount={6}
-      country="USA"
-      title="Life Lessons with Katie Zaferes"
-      price={136}
-
-    />
+    <App />
   </React.StrictMode>,
 )
